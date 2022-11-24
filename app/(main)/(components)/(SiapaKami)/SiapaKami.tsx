ae12d72f583d373a649da0575b0435bba8465abf
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-11-24 13:25:43 +0700

    menambahkan penjelasan siapa kami dan penjelasan logo

--- app/(main)/(components)/(SiapaKami)/SiapaKami.tsx
+++ app/(main)/(components)/(SiapaKami)/SiapaKami.tsx
@@ -1,4 +1,7 @@
 import styles from "./SiapaKami.module.css";
+import Image from "next/image";
+
+import penjelasanLogoImg from "../../../../public/penjelasan-logo.png";
 
 import { lato } from "../../../../lib/fonts";
 
@@ -9,21 +12,28 @@ export default function SiapaKami() {
       <div className="flex one two-1200">
         <div>
           <p className={styles.text}>
-            Lorem ipsum dolor sit amet, officia excepteur ex fugiat
-            reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
-            ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
-            Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
-            voluptate dolor minim nulla est proident. Nostrud officia pariatur
+            KIR adalah singkatan dari Kelompok Ilmiah Remaja, ekstrakurikuler
+            ini bertujuan untuk mengembangkan bakat dan minat siswa khususnya
+            dalam bidang ilmiah dan mengikuti kompetisi atau perlombaan. Latar
+            belakang terbentuknya ekstrakurikuler ini karena belum ada
+            organisasi yang fokus dibidang akademik atau keilmiahan.
           </p>
         </div>
         <div>
           <p className={styles.text}>
-            Lorem ipsum dolor sit amet, officia excepteur ex fugiat
-            reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
-            ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
-            Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
-            voluptate dolor minim nulla est proident. Nostrud officia pariatur
+            Tanggal dibentuk ekstrakurikuler KIR ini pada tanggal 16 Juli 2016,
+            diresmikan pada 21 Juli 2016. SCIO 12 diambil dari bahasa latin{" "}
+            <i>SCIO</i> yang memiliki arti <i>Aku Tahu</i>. Empat lingkaran
+            sebagai pengganti huruf "O" dengan masing-masing warna kuning, biru,
+            merah, dan hijau melambangkan setiap bidang studi.
           </p>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src={penjelasanLogoImg}
+            alt="Gambar yang berisi penjelasan makna dari warna logo ekstrakurikuler KIR."
+            width={920}
+          />
         </div>
       </div>
     </section>
